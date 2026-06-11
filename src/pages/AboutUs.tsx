897a1d7618ec3d6455aa9ab0cfb9ec5186955084
Chemi <abbyandam@gmail.com>
2026-06-11 14:50:21 -0500

    implemented home and about us pages

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,3 +1,42 @@
+import portrait from "../assets/about_us_portrait.png"
+import "../styles/aboutUs.scss"
+import { aboutUs } from "../globals"
+
 export default function AboutUs() {
-    return (<></>)
+    return (
+        <div className="about-us">
+            <div className="about-us-content">
+                    <div className="text">
+                        <h2>About Us</h2>
+                        <h4>Family Owned & Operated since 2019</h4>
+                        <img src={portrait}></img>
+                        <p>{aboutUs}</p>
+                    </div>
+                    <div className="portrait">
+                        <img src={portrait}></img>
+                    </div>
+            </div>
+            <div className="why-us">
+                <h4>Why Choose Rooma Tech Service?</h4>
+                <div className="content">
+                    <div>
+                        <h5>Family Owned & Run</h5>
+                        <p>You'll be speaking directly with the contractor doing the work</p>
+                    </div>
+                    <div>
+                        <h5>Work Done Right</h5>
+                        <p>Better work. Lower prices. No outsourcing</p>
+                    </div>
+                    <div>
+                        <h5>Luxury Service</h5>
+                        <p>High-end results with zero mess left behind</p>
+                    </div>
+                    <div>
+                        <h5>Faster Service</h5>
+                        <p>No waiting months for service. Get started in a week’s time</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,29 @@
+import '../styles/home.scss'
+import background from "../assets/home_image.jpg"
+import { FaPhoneAlt, FaTools, FaRegArrowAltCircleRight } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
 export default function Home() {
-    return (<></>)
+    return (
+    <div className='home'>
+        <img src={background} className='background-img'/>
+        <div className='home-text'>
+            <p><span className='big bold underline'>Commercial & Residential</span></p>
+            <h1>Home Improvement Services</h1>
+            <div>
+                <FaTools />
+                <p><span className='big bold italic tertiary'>Solutions are my specialty</span></p> 
+            </div>
+            <p><span className='bold'>HVAC・Electric・Plumbing・Kitchen・Bathroom</span></p>
+        </div>
+        <div className='buttons'>
+            <Link to={'/quote'} className='primary-button'><h2>Request a Quote</h2></Link>
+            <Link to={'/quote'} className='secondary-button'><h2>Contact</h2><FaPhoneAlt /></Link>
+        </div>
+        <Link to={'/services'} className='link'>
+            <span className='bold'>Get <span className='underline italic'>Free</span> Phone Troubleshooting</span>
+            <FaRegArrowAltCircleRight />
+        </Link>
+    </div>
+    )
 }
