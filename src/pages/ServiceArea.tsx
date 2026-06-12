9b6fe2e8db49b295fad57d7b394ba05f07330b2b
Chemi <abbyandam@gmail.com>
2026-06-12 13:05:48 -0500

    implemented service area page and fixed navbar wrapping

--- src/pages/ServiceArea.tsx
+++ src/pages/ServiceArea.tsx
@@ -1,3 +1,23 @@
+import { cities } from "../globals"
+import "../styles/serviceArea.scss"
+import map from "../assets/map.png"
+
 export default function ServiceArea() {
-    return (<></>)
+    return (
+        <div className="service-area">
+            <img src={map}/>
+            <h2>Service Area</h2>
+            <div className="gap"/>
+            <span className="tertiary"><h3>Pennsylvania</h3></span>
+            <hr/>
+            <ol>
+                { cities.sort().map((city) => (
+                    <p key={city}>{city}</p>
+                ))}
+            </ol>
+            <div className="gap"/>
+            <p><span className="bold">Not in our service area?</span></p>
+            <span className="tertiary"><p>Reach out anyway, we may still be able to help</p></span>
+        </div>
+    )
 }
