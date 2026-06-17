9474f8cbe8a190a0f7585c6a0aacaebe16c21e18
Chemi <abbyandam@gmail.com>
2026-06-16 20:59:12 -0500

    updated with business email template

--- api/formSubmission.post.ts
+++ api/formSubmission.post.ts
@@ -8,13 +8,20 @@ export default defineHandler(async (event) => {
 
     const name = formData.get('name') as string
     const email = formData.get('email') as string
+    const phone = formData.get('phone') as string
+    const address = formData.get('address') as string
+    const city = formData.get('city') as string
+    const state = formData.get('state') as string
+    const zipcode = formData.get('zipcode') as string
+    const service = formData.get('service') as string
+    const message = formData.get('message') as string
 
     const client = new BrevoClient({
         apiKey: useRuntimeConfig().brevoApiKey,
     });
 
-    
-    const response = await client.transactionalEmails.sendTransacEmail({
+    // send email to client confirming form submission
+    const client_response = await client.transactionalEmails.sendTransacEmail({
         to: [
             {
                 email: email,
@@ -24,6 +31,27 @@ export default defineHandler(async (event) => {
         templateId: 1,
     });
 
-    return response;
+    // send email to business alerting new submission
+    const business_response = await client.transactionalEmails.sendTransacEmail({
+        to: [
+            {
+                email: useRuntimeConfig().email,
+            },
+        ],
+        templateId: 2,
+        params: {
+            "NAME": name,
+            "PHONE": phone,
+            "EMAIL": email,
+            "ADDRESS": address,
+            "CITY": city,
+            "STATE": state,
+            "ZIPCODE": zipcode,
+            "SERVICE": service,
+            "MESSAGE": message
+        }
+    });
+
+    return {client_response: client_response, business_response: business_response};
 })
 
--- nitro.config.ts
+++ nitro.config.ts
@@ -3,6 +3,7 @@ import { defineNitroConfig } from 'nitro/config';
 export default defineNitroConfig({
     serverDir: './',
     runtimeConfig: {
-        brevoApiKey: ''
+        brevoApiKey: '',
+        email: ''
     }
 });
